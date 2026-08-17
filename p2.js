91b514b0669da4d00584d626a888f5f67530181f
foysal1138 <atikfoysal23@gmail.com>
2026-08-17 16:58:37 +0600

    add p2

---
+++ p2.js
@@ -0,0 +1,11 @@
+"use strict";
+/* একটি product-এর নাম, price, এবং availability store করো primitive types use করে।
+Input:
+name = "Laptop"
+price = 45000
+isAvailable = true
+Output:
+Laptop cost 45000 and is available: true
+*/
+let ProductName = "Laptop", price = 45000, isAvailable = true;
+console.log(`${ProductName} cost ${price} and is available: ${isAvailable}`);
---
+++ p2.ts
@@ -0,0 +1,11 @@
+/* একটি product-এর নাম, price, এবং availability store করো primitive types use করে।
+Input:
+name = "Laptop"
+price = 45000
+isAvailable = true
+Output:
+Laptop cost 45000 and is available: true
+*/
+
+let ProductName:String = "Laptop", price:number = 45000, isAvailable:boolean = true;
+console.log(`${ProductName} cost ${price} and is available: ${isAvailable}`)
