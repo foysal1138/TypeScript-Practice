604f85ba34bb0cc097c1a70210ae81117b9e0186
foysal1138 <atikfoysal23@gmail.com>
2026-08-17 16:49:53 +0600

    Start a new Journey with TypeScript

---
+++ p1.js
@@ -0,0 +1,12 @@
+"use strict";
+/*একটি function বানাও যা একটি user-এর নাম আর বয়স নিয়ে sentence print করবে।
+Input:
+printUser("Amina", 22)
+Output:
+Amina is 22 years old.
+*/
+function printUser(name, age) {
+    console.log(`${name} is ${age} years old.`);
+    return;
+}
+printUser("Atik", 24);
---
+++ p1.ts
@@ -0,0 +1,13 @@
+/*একটি function বানাও যা একটি user-এর নাম আর বয়স নিয়ে sentence print করবে।
+Input:
+printUser("Amina", 22)
+Output:
+Amina is 22 years old.
+*/
+
+function printUser(name:String,age:number):void{
+    console.log(`${name} is ${age} years old.`)
+    return 
+}
+
+printUser("Atik",24)
